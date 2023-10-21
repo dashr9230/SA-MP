344b46906ed19e53ddb7c31e6cb62878b8ff297c
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-10-22 07:36:58 +0800

    [launch3] Remove AfxEnableControlContainer

--- launch3/launch3.cpp
+++ launch3/launch3.cpp
@@ -41,8 +41,6 @@ CLaunch3App theApp;
 
 BOOL CLaunch3App::InitInstance()
 {
-	AfxEnableControlContainer();
-
 	// Standard initialization
 	// If you are not using these features and wish to reduce the size
 	//  of your final executable, you should remove from the following
