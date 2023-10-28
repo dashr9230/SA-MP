a779e730090069abe087c5c29ef03c24eeb4fd0b
unkcs <unkcstv@gmail.com>
2023-10-28 10:39:11 -0300

    [saco] Add CAudio stubs

---
+++ saco/game/audio.cpp
@@ -0,0 +1,36 @@
+#include "audio.h"
+
+void CAudio::GetRadioStation() 
+{
+    // TODO: CAudio::sub_100A2210 100A2210
+}
+
+void CAudio::StartRadio()
+{
+    // TODO: CAudio::sub_100A2240 100A2240
+}
+
+void CAudio::GetRadioVolume()
+{
+    // TODO: CAudio::sub_100A2280 100A2280
+}
+
+void CAudio::StopOutdoorAmbienceTrack()
+{
+    // TODO: CAudio::sub_100A2290 100A2290
+}
+
+void CAudio::SetOutdoorAmbienceTrack()
+{
+    // TODO: CAudio::sub_100A22A0 100A22A0
+}
+
+void CAudio::Play() 
+{
+    // TODO: CAudio::sub_100A22C0 100A22C0
+}
+
+void CAudio::IsOutdoorAmbienceTrackDisabled()
+{
+    // TODO: CAudio::sub_100A23A0 100A23A0
+}
