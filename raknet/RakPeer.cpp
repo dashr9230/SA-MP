0c5d0b5a212587723829573ddfa44e25c916a058
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-11-16 22:30:08 +0800

    [raknet] Add RakPeer vftables

---
+++ raknet/RakPeer.cpp
@@ -0,0 +1,348 @@
+// TODO: Implement RakPeer.cpp
+
+#include "RakPeer.h"
+
+void RakPeer::vftable_0()
+{
+	// TODO: RakPeer::vftable_0() (saco W: 10042780) (server W: 459CC0 L: 8072430) (bot W: 40D490 L: 807229E)
+}
+
+void RakPeer::vftable_4()
+{
+	// TODO: RakPeer::vftable_4() (saco W: 100427A0) (server W: 459CE0 L: 8072190) (bot W: 40D4B0 L: 80725E2)
+}
+
+void RakPeer::vftable_8()
+{
+	// TODO: RakPeer::vftable_8() (saco W: 10042A40) (server W: 459F80 L: 806EE80) (bot W: 40D750 L: 8072926)
+}
+
+void RakPeer::vftable_C()
+{
+	// TODO: RakPeer::vftable_C() (saco W: 10038400) (server W: 44FF00 L: 8076430) (bot W: 403EA0 L: 8072E54)
+}
+
+void RakPeer::vftable_10()
+{
+	// TODO: RakPeer::vftable_10() (saco W: 10038410) (server W: 44FF10 L: 806D180) (bot W: 403EB0 L: 8072FD2)
+}
+
+void RakPeer::vftable_14()
+{
+	// TODO: RakPeer::vftable_14() (saco W: 10038420) (server W: 44FF20 L: 806D1A0) (bot W: 403EC0 L: 8072FEE)
+}
+
+void RakPeer::vftable_18()
+{
+	// TODO: RakPeer::vftable_18() (saco W: 10038430) (server W: 44FF30 L: 806D1B0) (bot W: 403ED0 L: 8073008)
+}
+
+void RakPeer::vftable_1C()
+{
+	// TODO: RakPeer::vftable_1C() (saco W: 10038480) (server W: 44FF80 L: 806D1C0) (bot W: 403F20 L: 8073014)
+}
+
+void RakPeer::vftable_20()
+{
+	// TODO: RakPeer::vftable_20() (saco W: 10040550) (server W: 457B00 L: 806D230) (bot W: 40B2C0 L: 807306A)
+}
+
+void RakPeer::vftable_24()
+{
+	// TODO: RakPeer::vftable_24() (saco W: 10040620) (server W: 457BD0 L: 806FB70) (bot W: 40B390 L: 80730D2)
+}
+
+void RakPeer::vftable_28()
+{
+	// TODO: RakPeer::vftable_28() (saco W: 1003E040) (server W: 455330 L: 8071070) (bot W: 408FE0 L: 80731E2)
+}
+
+void RakPeer::vftable_2C()
+{
+	// TODO: RakPeer::vftable_2C() (saco W: 100384D0) (server W: 44FFD0 L: 8076E70) (bot W: 403F70 L: 8080FB0)
+}
+
+void RakPeer::vftable_30()
+{
+	// TODO: RakPeer::vftable_30() (saco W: 1003C3D0) (server W: 4536E0 L: 806D2B0) (bot W: 407380 L: 8073546)
+}
+
+void RakPeer::vftable_34()
+{
+	// TODO: RakPeer::vftable_34() (saco W: 1003C2C0) (server W: 4535D0 L: 806EBA0) (bot W: 407270 L: 8073696)
+}
+
+void RakPeer::vftable_38()
+{
+	// TODO: RakPeer::vftable_38() (saco W: 10040FD0) (server W: 458510 L: 806EA30) (bot W: 40BCE0 L: 8073806)
+}
+
+void RakPeer::vftable_3C()
+{
+	// TODO: RakPeer::vftable_3C() (saco W: 100385B0) (server W: 4500B0 L: 8071D60) (bot W: 404050 L: 80739A2)
+}
+
+void RakPeer::vftable_40()
+{
+	// TODO: RakPeer::vftable_40() (saco W: 100385E0) (server W: 4500E0 L: 806D480) (bot W: 404080 L: 8073CD2)
+}
+
+void RakPeer::vftable_44()
+{
+	// TODO: RakPeer::vftable_44() (saco W: 100385F0) (server W: 4500F0 L: 806D4C0) (bot W: 404090 L: 8073D0E)
+}
+
+void RakPeer::vftable_48()
+{
+	// TODO: RakPeer::vftable_48() (saco W: 10038620) (server W: 450120 L: 806D4D0) (bot W: 4040C0 L: 8073D1A)
+}
+
+void RakPeer::vftable_4C()
+{
+	// TODO: RakPeer::vftable_4C() (saco W: 10038650) (server W: 450150 L: 806D520) (bot W: 4040F0 L: 8073D62)
+}
+
+void RakPeer::vftable_50()
+{
+	// TODO: RakPeer::vftable_50() (saco W: 1003A720) (server W: 451A60 L: 806D580) (bot W: 405750 L: 8073DAA)
+}
+
+void RakPeer::vftable_54()
+{
+	// TODO: RakPeer::vftable_54() (saco W: 1003E050) (server W: 455340 L: 8071710) (bot W: 408FF0 L: 8073DB6)
+}
+
+void RakPeer::vftable_58()
+{
+	// TODO: RakPeer::vftable_58() (saco W: 10040A00) (server W: 457FB0 L: 806D590) (bot W: 40B770 L: 80741A0)
+}
+
+void RakPeer::vftable_5C()
+{
+	// TODO: RakPeer::vftable_5C() (saco W: 1003E330) (server W: 455620 L: 8074290) (bot W: 4092D0 L: 80742D2)
+}
+
+void RakPeer::vftable_60()
+{
+	// TODO: RakPeer::vftable_60() (saco W: 10038660) (server W: 450160 L: 80716E0) (bot W: 404100 L: 8074318)
+}
+
+void RakPeer::vftable_64()
+{
+	// TODO: RakPeer::vftable_64() (saco W: 1003A7D0) (server W: 451B10 L: 806D6B0) (bot W: 405800 L: 8074342)
+}
+
+void RakPeer::vftable_68()
+{
+	// TODO: RakPeer::vftable_68() (saco W: 1003C4D0) (server W: 4537E0 L: 806D770) (bot W: 407480 L: 807441A)
+}
+
+void RakPeer::vftable_6C()
+{
+	// TODO: RakPeer::vftable_6C() (saco W: 1003A960) (server W: 451CA0 L: 806F440) (bot W: 405990 L: 80745D6)
+}
+
+void RakPeer::vftable_70()
+{
+	// TODO: RakPeer::vftable_70() (saco W: 1003C5E0) (server W: 4538F0 L: 806D8E0) (bot W: 407590 L: 807473C)
+}
+
+void RakPeer::vftable_74()
+{
+	// TODO: RakPeer::vftable_74() (saco W: 1003A9D0) (server W: 451D10 L: 806F300) (bot W: 405A00 L: 80747FC)
+}
+
+void RakPeer::vftable_78()
+{
+	// TODO: RakPeer::vftable_78() (saco W: 10040A30) (server W: 457FE0 L: 8070970) (bot W: 40B7A0 L: 8074A70)
+}
+
+void RakPeer::vftable_7C()
+{
+	// TODO: RakPeer::vftable_7C() (saco W: 1003E350) (server W: 455640 L: 806D9A0) (bot W: 4092F0 L: 8074A9A)
+}
+
+void RakPeer::vftable_80()
+{
+	// TODO: RakPeer::vftable_80() (saco W: 1003E3B0) (server W: 4556A0 L: 8070270) (bot W: 409350 L: 8074CB4)
+}
+
+void RakPeer::vftable_84()
+{
+	// TODO: RakPeer::vftable_84() (saco W: 1003E400) (server W: 4556F0 L: 8070210) (bot W: 4093A0 L: 8074D7A)
+}
+
+void RakPeer::vftable_88()
+{
+	// TODO: RakPeer::vftable_88() (saco W: 10038720) (server W: 450220 L: 80701D0) (bot W: 404160 L: 8074DFE)
+}
+
+void RakPeer::vftable_8C()
+{
+	// TODO: RakPeer::vftable_8C() (saco W: 1003E440) (server W: 455730 L: 806DBB0) (bot W: 4093E0 L: 8074E52)
+}
+
+void RakPeer::vftable_90()
+{
+	// TODO: RakPeer::vftable_90() (saco W: 1003E490) (server W: 455780 L: 8070160) (bot W: 409430 L: 8074E6A)
+}
+
+void RakPeer::vftable_94()
+{
+	// TODO: RakPeer::vftable_94() (saco W: 10040A50) (server W: 458000 L: 80700B0) (bot W: 40B7C0 L: 8074EE2)
+}
+
+void RakPeer::vftable_98()
+{
+	// TODO: RakPeer::vftable_98() (saco W: 10038730) (server W: 450230 L: 8070B60) (bot W: 404170 L: 8074FA8)
+}
+
+void RakPeer::vftable_9C()
+{
+	// TODO: RakPeer::vftable_9C() (saco W: 10038780) (server W: 450280 L: 806DBC0) (bot W: 4041C0 L: 8074FD2)
+}
+
+void RakPeer::vftable_A0()
+{
+	// TODO: RakPeer::vftable_A0() (saco W: 100387A0) (server W: 4502A0 L: 806DC50) (bot W: 4041E0 L: 8075034)
+}
+
+void RakPeer::vftable_A4()
+{
+	// TODO: RakPeer::vftable_A4() (saco W: 1003E520) (server W: 455810 L: 806DC70) (bot W: 4094C0 L: 8075056)
+}
+
+void RakPeer::vftable_A8()
+{
+	// TODO: RakPeer::vftable_A8() (saco W: 10038860) (server W: 450360 L: 8070050) (bot W: 4042A0 L: 807514E)
+}
+
+void RakPeer::vftable_AC()
+{
+	// TODO: RakPeer::vftable_AC() (saco W: 100388B0) (server W: 4503B0 L: 806DD50) (bot W: 4042F0 L: 807519E)
+}
+
+void RakPeer::vftable_B0()
+{
+	// TODO: RakPeer::vftable_B0() (saco W: 100388C0) (server W: 4503C0 L: 806DDB0) (bot W: 404300 L: 80751FE)
+}
+
+void RakPeer::vftable_B4()
+{
+	// TODO: RakPeer::vftable_B4() (saco W: 10038920) (server W: 450420 L: 806DDC0) (bot W: 404360 L: 807520C)
+}
+
+void RakPeer::vftable_B8()
+{
+	// TODO: RakPeer::vftable_B8() (saco W: 10038910) (server W: 450410 L: 806DE40) (bot W: 404350 L: 807529A)
+}
+
+void RakPeer::vftable_BC()
+{
+	// TODO: RakPeer::vftable_BC() (saco W: 10038CB0) (server W: 450790 L: 806DE30) (bot W: 4046D0 L: 8075280)
+}
+
+void RakPeer::vftable_C0()
+{
+	// TODO: RakPeer::vftable_C0() (saco W: 10038960) (server W: 450460 L: 806E690) (bot W: 4043A0 L: 8075F86)
+}
+
+void RakPeer::vftable_C4()
+{
+	// TODO: RakPeer::vftable_C4() (saco W: 1003AB30) (server W: 451E70 L: 806DEA0) (bot W: 405B60 L: 80752EC)
+}
+
+void RakPeer::vftable_C8()
+{
+	// TODO: RakPeer::vftable_C8() (saco W: 10038970) (server W: 450470 L: 806DEC0) (bot W: 4043B0 L: 8075308)
+}
+
+void RakPeer::vftable_CC()
+{
+	// TODO: RakPeer::vftable_CC() (saco W: 100389C0) (server W: 4504C0 L: 806E110) (bot W: 404400 L: 807558A)
+}
+
+void RakPeer::vftable_D0()
+{
+	// TODO: RakPeer::vftable_D0() (saco W: 10038A10) (server W: 450510 L: 806E170) (bot W: 404450 L: 80755EA)
+}
+
+void RakPeer::vftable_D4()
+{
+	// TODO: RakPeer::vftable_D4() (saco W: 10038A20) (server W: 450520 L: 806E1D0) (bot W: 404460 L: 807564A)
+}
+
+void RakPeer::vftable_D8()
+{
+	// TODO: RakPeer::vftable_D8() (saco W: 10038A60) (server W: 450560 L: 806E1F0) (bot W: 4044A0 L: 8075666)
+}
+
+void RakPeer::vftable_DC()
+{
+	// TODO: RakPeer::vftable_DC() (saco W: 1003ACA0) (server W: 451FE0 L: 806E250) (bot W: 405CD0 L: 80756CC)
+}
+
+void RakPeer::vftable_E0()
+{
+	// TODO: RakPeer::vftable_E0() (saco W: 10038B50) (server W: 450630 L: 806E330) (bot W: 404570 L: 80757FA)
+}
+
+void RakPeer::vftable_E4()
+{
+	// TODO: RakPeer::vftable_E4() (saco W: 10038B90) (server W: 450670 L: 806E3C0) (bot W: 4045B0 L: 80758BE)
+}
+
+void RakPeer::vftable_E8()
+{
+	// TODO: RakPeer::vftable_E8() (saco W: 1003AD20) (server W: 452060 L: 806E400) (bot W: 405D50 L: 8075910)
+}
+
+void RakPeer::vftable_EC()
+{
+	// TODO: RakPeer::vftable_EC() (saco W: 1003AD60) (server W: 4520A0 L: 806E440) (bot W: 405D90 L: 8075962)
+}
+
+void RakPeer::vftable_F0()
+{
+	// TODO: RakPeer::vftable_F0() (saco W: 1003ADB0) (server W: 4520F0 L: 806E4A0) (bot W: 405DE0 L: 80759B4)
+}
+
+void RakPeer::vftable_F4()
+{
+	// TODO: RakPeer::vftable_F4() (saco W: 10038BD0) (server W: 4506B0 L: 806E510) (bot W: 4045F0 L: 8075A76)
+}
+
+void RakPeer::vftable_F8()
+{
+	// TODO: RakPeer::vftable_F8() (saco W: 10038BE0) (server W: 4506C0 L: 806E560) (bot W: 404600 L: 8075AC2)
+}
+
+void RakPeer::vftable_FC()
+{
+	// TODO: RakPeer::vftable_FC() (saco W: 10038C00) (server W: 4506E0 L: 806E580) (bot W: 404620 L: 8075AD4)
+}
+
+void RakPeer::vftable_100()
+{
+	// TODO: RakPeer::vftable_100() (saco W: 10038C80) (server W: 450760 L: 806E5A0) (bot W: 4046A0 L: 8075AF4)
+}
+
+void RakPeer::vftable_104()
+{
+	// TODO: RakPeer::vftable_104() (saco W: 1003E5C0) (server W: 4558B0 L: 806E650) (bot W: 409560 L: 8075B92)
+}
+
+void RakPeer::vftable_108()
+{
+	// TODO: RakPeer::vftable_108() (saco W: 1003EC80) (server W: 456030 L: 806FC90) (bot W: 409C20 L: 8075C52)
+}
+
+void RakPeer::vftable_10C()
+{
+	// TODO: RakPeer::vftable_10C() (saco W: 1003E560) (server W: 455850 L: 806F710) (bot W: 409500 L: 8076B8C)
+}
+
+void RakPeer::vftable_110()
+{
+	// TODO: RakPeer::vftable_110() (server L: 806FFE0) (bot L: 8075BDC)
+}
---
+++ raknet/RakPeer.h
@@ -0,0 +1,84 @@
+// TODO: Implement RakPeer.h
+
+#ifndef __RAK_PEER_H
+#define __RAK_PEER_H
+
+#include "Export.h"
+#include "RakPeerInterface.h"
+
+class RAK_DLL_EXPORT RakPeer : public RakPeerInterface
+{
+public:
+
+	void vftable_0();
+	void vftable_4();
+	void vftable_8();
+	void vftable_C();
+	void vftable_10();
+	void vftable_14();
+	void vftable_18();
+	void vftable_1C();
+	void vftable_20();
+	void vftable_24();
+	void vftable_28();
+	void vftable_2C();
+	void vftable_30();
+	void vftable_34();
+	void vftable_38();
+	void vftable_3C();
+	void vftable_40();
+	void vftable_44();
+	void vftable_48();
+	void vftable_4C();
+	void vftable_50();
+	void vftable_54();
+	void vftable_58();
+	void vftable_5C();
+	void vftable_60();
+	void vftable_64();
+	void vftable_68();
+	void vftable_6C();
+	void vftable_70();
+	void vftable_74();
+	void vftable_78();
+	void vftable_7C();
+	void vftable_80();
+	void vftable_84();
+	void vftable_88();
+	void vftable_8C();
+	void vftable_90();
+	void vftable_94();
+	void vftable_98();
+	void vftable_9C();
+	void vftable_A0();
+	void vftable_A4();
+	void vftable_A8();
+	void vftable_AC();
+	void vftable_B0();
+	void vftable_B4();
+	void vftable_B8();
+	void vftable_BC();
+	void vftable_C0();
+	void vftable_C4();
+	void vftable_C8();
+	void vftable_CC();
+	void vftable_D0();
+	void vftable_D4();
+	void vftable_D8();
+	void vftable_DC();
+	void vftable_E0();
+	void vftable_E4();
+	void vftable_E8();
+	void vftable_EC();
+	void vftable_F0();
+	void vftable_F4();
+	void vftable_F8();
+	void vftable_FC();
+	void vftable_100();
+	void vftable_104();
+	void vftable_108();
+	void vftable_10C();
+	void vftable_110();
+};
+
+#endif
