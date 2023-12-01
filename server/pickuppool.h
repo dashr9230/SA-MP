decfd66b772890eb07598bd839eda776992f0975
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-12-01 20:46:24 +0800

    [server] Implement CPickupPool ctor/dtor

--- server/pickuppool.h
+++ server/pickuppool.h
@@ -2,10 +2,30 @@
 #ifndef SAMPSRV_PICKUPPOOL_H
 #define SAMPSRV_PICKUPPOOL_H
 
+#define MAX_PICKUPS 4096
+
 class CPickupPool // size: WL 114692
 {
 private:
-	char _gap0[114692];
+
+	char gap0[81920];
+	int field_14000[4096];
+	int field_18000[4096];
+	int field_1C000;
+
+public:
+
+	CPickupPool() {
+		for (int i = 0; i < MAX_PICKUPS; i++)
+		{
+			field_14000[i] = 0;
+			field_18000[i] = 0;
+		}
+		field_1C000 = 0;
+	};
+
+	~CPickupPool() {};
+
 };
 
 #endif
