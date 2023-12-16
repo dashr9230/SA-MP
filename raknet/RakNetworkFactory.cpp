118153f727bcae0d414bcc819fb21370f225f5f7
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-12-16 23:25:47 +0800

    [raknet] Update RakNetworkFactory
    This commit implements:
    * GetRakClientInterface()
    * DestroyRakClientInterface()
    * DestroyRakServerInterface()
    * DestroyRakPeerInterface()

--- raknet/RakNetworkFactory.cpp
+++ raknet/RakNetworkFactory.cpp
@@ -1,13 +1,16 @@
-// TODO: Implement RakNetworkFactory.cpp
 
 #include "RakNetworkFactory.h"
 #include "RakServerInterface.h"
-
+#include "RakClientInterface.h"
 #include "RakServer.h"
-
+#include "RakClient.h"
 #include "RakPeerInterface.h"
 #include "RakPeer.h"
 
+RakClientInterface* RakNetworkFactory::GetRakClientInterface( void )
+{
+	return new RakClient;
+}
 RakServerInterface* RakNetworkFactory::GetRakServerInterface( void )
 {
 	return new RakServer;
@@ -15,4 +18,20 @@ RakServerInterface* RakNetworkFactory::GetRakServerInterface( void )
 RakPeerInterface* RakNetworkFactory::GetRakPeerInterface( void )
 {
 	return new RakPeer;
+}
+
+
+
+
+void RakNetworkFactory::DestroyRakClientInterface( RakClientInterface* i )
+{
+	delete ( RakClient* ) i;
+}
+void RakNetworkFactory::DestroyRakServerInterface( RakServerInterface* i )
+{
+	delete ( RakServer* ) i;
+}
+void RakNetworkFactory::DestroyRakPeerInterface( RakPeerInterface* i )
+{
+	delete ( RakPeer* ) i;
 }
--- raknet/RakNetworkFactory.h
+++ raknet/RakNetworkFactory.h
@@ -1,11 +1,10 @@
 
-// TODO: Implement RakNetworkFactory.h
-
 #ifndef __RAK_NETWORK_FACTORY_H
 #define __RAK_NETWORK_FACTORY_H
 
 #include "Export.h"
 
+class RakClientInterface;
 class RakServerInterface;
 class RakPeerInterface;
 
@@ -13,9 +12,13 @@ class RAK_DLL_EXPORT RakNetworkFactory
 {
 public:
 
+	static RakClientInterface* GetRakClientInterface( void );
 	static RakServerInterface* GetRakServerInterface( void );
 	static RakPeerInterface* GetRakPeerInterface( void );
 
+	static void DestroyRakClientInterface( RakClientInterface* i );
+	static void DestroyRakServerInterface( RakServerInterface* i );
+	static void DestroyRakPeerInterface( RakPeerInterface* i );
 };
 
 #endif
