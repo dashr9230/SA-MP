08c62016bd3bc9c4249561d065e0943699946a06
RD42 <42702181+dashr9230@users.noreply.github.com>
2023-12-18 23:36:33 +0800

    [exgui] Update samp.dpr

--- exgui/samp.dpr
+++ exgui/samp.dpr
@@ -1,16 +1,18 @@
 program samp;
 
+
+
 uses
+  Windows,
   Forms,
   Main in 'Main.pas' {fmMain},
   About in 'About.pas' {fmAbout},
-  ExportFavorites in 'ExportFavorites.pas' {fmExportFavorites},
-  ImportFavorites in 'ImportFavorites.pas' {fmImportFavorites},
-  MasterUpdate in 'MasterUpdate.pas' {fmMasterUpdate},
   RconConfig in 'RconConfig.pas' {fmRconConfig},
-  ServerProperties in 'ServerProperties.pas' {fmServerProperties},
   Settings in 'Settings.pas' {fmSettings},
-  Rcon in 'Rcon.pas' {fmRcon},
+  ServerProperties in 'ServerProperties.pas' {fmServerProperties},
+  ImportFavorites in 'ImportFavorites.pas' {fmImportFavorites},
+  ExportFavorites in 'ExportFavorites.pas' {fmExportFavorites},
+  MasterUpdate in 'MasterUpdate.pas' {fmMasterUpdate},
   unit_webrunform in 'unit_webrunform.pas' {wnd_webrunform};
 
 {$R *.res}
@@ -20,13 +22,7 @@ begin
   Application.Title := 'SA-MP 0.3';
   Application.CreateForm(TfmMain, fmMain);
   Application.CreateForm(TfmAbout, fmAbout);
-  Application.CreateForm(TfmExportFavorites, fmExportFavorites);
-  Application.CreateForm(TfmImportFavorites, fmImportFavorites);
   Application.CreateForm(TfmMasterUpdate, fmMasterUpdate);
-  Application.CreateForm(TfmRconConfig, fmRconConfig);
-  Application.CreateForm(TfmServerProperties, fmServerProperties);
-  Application.CreateForm(TfmSettings, fmSettings);
-  Application.CreateForm(TfmRcon, fmRcon);
   Application.CreateForm(Twnd_webrunform, wnd_webrunform);
   Application.Run;
 end.
