94a25da90d2bee7e43c83ae3c6a7922575d7484a
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-01-06 23:36:46 +0800

    [saco] Implement SubclassGameWindow
    - Adds NewWndProc stub

---
+++ saco/subclass.cpp
@@ -0,0 +1,48 @@
+
+#include "main.h"
+
+extern CGame			*pGame;
+
+WNDPROC hOldProc;
+LRESULT APIENTRY NewWndProc(HWND,UINT,WPARAM,LPARAM);
+
+//----------------------------------------------------
+
+BOOL SubclassGameWindow()
+{
+	HWND hwndGameWnd = pGame->GetMainWindowHwnd();
+
+	if(!hwndGameWnd) return FALSE;
+
+	DWORD dwStyle = GetClassLong(hwndGameWnd,GCL_STYLE);
+	SetClassLong(hwndGameWnd,GCL_STYLE,dwStyle|CS_DBLCLKS);
+
+	if(hwndGameWnd) {
+		hOldProc = (WNDPROC)GetWindowLong(hwndGameWnd,GWL_WNDPROC);
+		if(hOldProc != NewWndProc) {
+			SetWindowLong(hwndGameWnd,GWL_WNDPROC,(LONG)NewWndProc);
+		}
+	}
+
+	SetWindowText(hwndGameWnd,"GTA:SA:MP");
+
+	if(IsWindowUnicode(hwndGameWnd)) {
+		OutputDebugString("GTA is unicode");
+	} else {
+		OutputDebugString("GTA is not unicode");
+	}
+
+	return TRUE;
+}
+
+//----------------------------------------------------
+
+LRESULT APIENTRY NewWndProc( HWND hwnd,UINT uMsg,
+							 WPARAM wParam,LPARAM lParam ) 
+{
+	// TODO: NewWndProc
+
+	return CallWindowProc(hOldProc,hwnd,uMsg,wParam,lParam);
+}
+
+//----------------------------------------------------
