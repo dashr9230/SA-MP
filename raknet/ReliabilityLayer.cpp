e1fc24b7685f98076c6fcd52056abaf47580c01c
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-02-01 00:14:32 +0800

    [raknet] Add ReliabilityLayer ctor as stub
    * Adds `ReliabilityLayer::InitializeVariable()` stub

---
+++ raknet/ReliabilityLayer.cpp
@@ -0,0 +1,15 @@
+// TODO: Implement ReliabilityLayer.cpp
+
+#include "ReliabilityLayer.h"
+
+ReliabilityLayer::ReliabilityLayer()
+{
+	// TODO: ReliabilityLayer::ReliabilityLayer
+
+	InitializeVariables();
+}
+
+void ReliabilityLayer::InitializeVariables( void )
+{
+	// TODO: ReliabilityLayer::InitializeVariables
+}
---
+++ raknet/ReliabilityLayer.h
@@ -0,0 +1,20 @@
+// TODO: Implement ReliabilityLayer.h
+
+#ifndef __RELIABILITY_LAYER_H
+#define __RELIABILITY_LAYER_H
+
+#include "SocketLayer.h"
+
+class ReliabilityLayer
+{
+public:
+
+	/// Constructor
+	ReliabilityLayer();
+
+	// Initialize the variables
+	void InitializeVariables( void );
+
+};
+
+#endif
