2db87fb94edb83244f0b9d3db186091a6c5f5441
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-01-31 23:55:30 +0800

    [saco] Add `CChatWindow::AddDebugMessage` stub

--- saco/chatwindow.cpp
+++ saco/chatwindow.cpp
@@ -1,7 +1,12 @@
 
-#include "chatwindow.h"
+#include "main.h"
 
 CChatWindow::CChatWindow()
 {
 	// TODO: CChatWindow::CChatWindow() .text:100680F0
+}
+
+void CChatWindow::AddDebugMessage(CHAR * szFormat, ...)
+{
+	// TODO: CChatWindow::AddDebugMessage .text:100680F0
 }
--- saco/chatwindow.h
+++ saco/chatwindow.h
@@ -8,4 +8,6 @@ private:
 
 public:
 	CChatWindow();
+
+	void AddDebugMessage(CHAR *szFormat, ...);
 };
