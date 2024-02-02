99b6dd3eda480a4b09d8c7e14d9681e5dc985a67
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-02-02 22:47:18 +0800

    [saco] Implement `CCmdWindow::AddDefaultCmdProc`

--- saco/cmdwindow.cpp
+++ saco/cmdwindow.cpp
@@ -1,7 +1,16 @@
 
-#include "cmdwindow.h"
+#include "main.h"
 
 CCmdWindow::CCmdWindow()
 {
 	// TODO: CCmdWindow::CCmdWindow() .text:100693D0
 }
+
+//----------------------------------------------------
+
+void CCmdWindow::AddDefaultCmdProc(CMDPROC cmdDefault)
+{
+	m_pDefaultCmd = cmdDefault;	
+}
+
+//----------------------------------------------------
--- saco/cmdwindow.h
+++ saco/cmdwindow.h
@@ -1,11 +1,20 @@
 
 #pragma once
 
+typedef void (__cdecl *CMDPROC)(PCHAR);
+
 class CCmdWindow // size: 6908
 {
 private:
 	char _gap0[6908];
 
 public:
+
+	CMDPROC		m_pDefaultCmd;	 // used when no command specifier was
+								 // used (ie. a normal chat message)
+
+
 	CCmdWindow();
+
+	void AddDefaultCmdProc(CMDPROC cmdDefault);
 };
