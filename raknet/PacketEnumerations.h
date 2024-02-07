9ec91cdd26f670ed9692fab48c469358c02bb3e1
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-02-07 23:31:27 +0800

    [raknet] Update PacketEnumerations.h
    * Adds more packet IDs
    * Adds comments to the packet IDs

--- raknet/PacketEnumerations.h
+++ raknet/PacketEnumerations.h
@@ -1,68 +1,194 @@
+/// \file
+/// \brief All the packet identifiers used by RakNet.  Packet identifiers comprise the first byte of any message.
+///
+/// This file is part of RakNet Copyright 2003 Kevin Jenkins.
+///
+/// Usage of RakNet is subject to the appropriate license agreement.
+/// Creative Commons Licensees are subject to the
+/// license found at
+/// http://creativecommons.org/licenses/by-nc/2.5/
+/// Single application licensees are subject to the license found at
+/// http://www.rakkarsoft.com/SingleApplicationLicense.html
+/// Custom license users are subject to the terms therein.
+/// GPL license users are subject to the GNU General Public
+/// License as published by the Free
+/// Software Foundation; either version 2 of the License, or (at your
+/// option) any later version.
 
 #ifndef __PACKET_ENUMERATIONS_H
-#define __PACKET_ENUMERATIONS_H
+#define __PACKET_ENUMERATIONS_H 
 
+/// You should not edit the file PacketEnumerations.h as it is a part of RakNet static library
+/// To define your own message id, define an enum following the code example that follows. 
+///
+/// \code
+/// enum {
+///   ID_MYPROJECT_MSG_1 = ID_USER_PACKET_ENUM
+///   ID_MYPROJECT_MSG_2, 
+///    ... 
+/// };
+/// \endcode 
+///
+/// \note All these enumerations should be casted to (unsigned char) before writing them to RakNet::BitStream
 enum
 {
 	// TODO: PacketEnumerations.h is not complete yet
 
-	/*
-		Notes:
-		- Apperently there are two ID_NEW_INCOMING_CONNECTION. 24 and 30
-		  It is certain that ID 30 is the real ID_NEW_INCOMING_CONNECTION, because
-		  RakNet uses this ID for ReplicaManager::OnReceive, ConnectionGraph::OnReceive, etc...
-		  Packet ID 24 only used in CNetGame::Process so far. Maybe there are auth packets between these IDs?
-	*/
+	/// 6: Ping from a connected system.  Update timestamps (internal use only)
+	ID_INTERNAL_PING = 6,  
+	/// 7: Ping from an unconnected system.  Reply but do not update timestamps. (internal use only)
+	ID_PING,
+	/// 8: Ping from an unconnected system.  Only reply if we have open connections. Do not update timestamps. (internal use only)
+	ID_PING_OPEN_CONNECTIONS,
 
-	ID_UNK_12_SYNC = 12, // TODO: ID_UNK_12_SYNC
+	/// 15: Connecting to a secured server/peer
+	ID_SECURED_CONNECTION_RESPONSE = 15,
+	/// 16: Connecting to a secured server/peer
+	ID_SECURED_CONNECTION_CONFIRMATION,
 
-	ID_NEW_INCOMING_CONNECTION_2 = 24,
+	/// 19: Server / Client only - The server is broadcasting a random number seed (internal use only)
+	ID_SET_RANDOM_NUMBER_SEED = 19,
+	/// 20: Remote procedure call (internal use only)
+	ID_RPC,
 
+	/// [PEER|SERVER|CLIENT] 29: Sent to the player when a connection request cannot be completed due to inability to connect.
+	/// Never transmitted.
 	ID_CONNECTION_ATTEMPT_FAILED = 29,
-	ID_NEW_INCOMING_CONNECTION = 30,
-	ID_NO_FREE_INCOMING_CONNECTIONS = 31,
-	ID_DISCONNECTION_NOTIFICATION = 32,
-	ID_CONNECTION_LOST = 33,
-	ID_CONNECTION_REQUEST_ACCEPTED = 34,
-	ID_RSA_PUBLIC_KEY_MISMATCH = 35,
-	ID_CONNECTION_BANNED = 36,
-	ID_INVALID_PASSWORD = 37,
-	ID_MODIFIED_PACKET = 38,
 
-	ID_TIMESTAMP = 40,
+	/// [PEER|SERVER] 30: A remote system has successfully connected.
+	ID_NEW_INCOMING_CONNECTION,
 
+	/// [PEER|CLIENT] 31: The system we attempted to connect to is not accepting new connections.
+	ID_NO_FREE_INCOMING_CONNECTIONS,
+
+	/// [PEER|SERVER|CLIENT] 32: The system specified in Packet::playerID has disconnected from us.  For the client, this would mean the server has shutdown.
+	ID_DISCONNECTION_NOTIFICATION,
+
+	/// [PEER|SERVER|CLIENT] 33: Reliable packets cannot be delivered to the system specifed in Packet::playerID.  The connection to that system has been closed.
+	ID_CONNECTION_LOST,
+
+	/// [PEER|CLIENT] 34: In a client/server environment, our connection request to the server has been accepted.
+	ID_CONNECTION_REQUEST_ACCEPTED,
+
+	/// [CLIENT|PEER] 35: We preset an RSA public key which does not match what the system we connected to is using.
+	ID_RSA_PUBLIC_KEY_MISMATCH,
+
+	/// [PEER|CLIENT] 36: We are banned from the system we attempted to connect to.
+	ID_CONNECTION_BANNED,
+
+	/// [PEER|CLIENT] 37: The remote system is using a password and has refused our connection because we did not set the correct password.
+	ID_INVALID_PASSWORD,
+
+	/// [PEER|SERVER|CLIENT] 38: A packet has been tampered with in transit.  The sender is contained in Packet::playerID.
+	/// Never transmitted.
+	ID_MODIFIED_PACKET,
+
+	/// [PEER] 39: Pong from an unconnected system.  First byte is ID_PONG, second sizeof(RakNetTime) bytes is the ping, following bytes is system specific enumeration data.
+	ID_PONG,
+
+	/// [PEER|SERVER|CLIENT] 40: The four bytes following this byte represent an unsigned int which is automatically modified by the difference in system times between the sender and the recipient. Requires that you call StartOccasionalPing.
+	ID_TIMESTAMP,
+
+
+	/// [CLIENT] 42: In a client/server environment, a client other than ourselves has disconnected gracefully.  Packet::playerID is modified to reflect the playerID of this client.
 	ID_REMOTE_DISCONNECTION_NOTIFICATION = 42,
-	ID_REMOTE_CONNECTION_LOST = 43,
 
-	ID_CONNECTION_GRAPH_REQUEST = 57,
-	ID_CONNECTION_GRAPH_REPLY = 58,
-	ID_CONNECTION_GRAPH_UPDATE = 59,
-	ID_CONNECTION_GRAPH_NEW_CONNECTION = 60,
+	/// [CLIENT] 43: In a client/server environment, a client other than ourselves has been forcefully dropped. Packet::playerID is modified to reflect the playerID of this client.
+	ID_REMOTE_CONNECTION_LOST,
 
-	ID_CONNECTION_GRAPH_CONNECTION_LOST = 61,
-	ID_CONNECTION_GRAPH_DISCONNECTION_NOTIFICATION = 62,
+	/// [FILELIST] 47:
+	ID_FILE_LIST_TRANSFER_HEADER = 47,
 
-	ID_REPLICA_MANAGER_CONSTRUCTION = 63,
-	ID_REPLICA_MANAGER_DESTRUCTION = 64,
-	ID_REPLICA_MANAGER_SCOPE_CHANGE = 65,
-	ID_REPLICA_MANAGER_SERIALIZE = 66,
-	ID_REPLICA_MANAGER_DOWNLOAD_COMPLETE = 67,
+	/// [FILELIST] 48:
+	ID_FILE_LIST_TRANSFER_FILE,
 
-	// SA-MP
-	// Found in L: 08067810 - Thanks Kalcor!
+	/// [Delta Directory transfer] 49:
+	ID_DDT_DOWNLOAD_REQUEST,
+
+	/// [PEER|SERVER|CLIENT] 55: Inform a remote system of our IP/Port.
+	ID_ADVERTISE_SYSTEM = 55,
+
+	/// [RakNetTransport] 56
+	ID_TRANSPORT_STRING,
+
+	/// [ConnectionGraph] 57
+	ID_CONNECTION_GRAPH_REQUEST,
+	/// [ConnectionGraph] 58
+	ID_CONNECTION_GRAPH_REPLY,
+	/// [ConnectionGraph] 59
+	ID_CONNECTION_GRAPH_UPDATE,
+	/// [ConnectionGraph] 60
+	ID_CONNECTION_GRAPH_NEW_CONNECTION,
+	/// [ConnectionGraph] 61
+	ID_CONNECTION_GRAPH_CONNECTION_LOST,
+	/// [ConnectionGraph] 62
+	ID_CONNECTION_GRAPH_DISCONNECTION_NOTIFICATION,
+
+	/// [ReplicaManager] 63
+	ID_REPLICA_MANAGER_CONSTRUCTION,
+	/// [ReplicaManager] 64
+	ID_REPLICA_MANAGER_DESTRUCTION,
+	/// [ReplicaManager] 65
+	ID_REPLICA_MANAGER_SCOPE_CHANGE,
+	/// [ReplicaManager] 66
+	ID_REPLICA_MANAGER_SERIALIZE,
+	/// [ReplicaManager] 67
+	ID_REPLICA_MANAGER_DOWNLOAD_COMPLETE,
+
+	/// [Router] 68
+	ID_ROUTE_AND_MULTICAST,
+
+	/// [NAT Punchthrough]
+	ID_NAT_PUNCHTHROUGH_REQUEST = 81,
+
+	/// [NAT Punchthrough] Returned to user.  PlayerID binary address / port is written to the stream
+	ID_NAT_TARGET_CONNECTION_LOST,
+
+	/// [NAT Punchthrough] Returned to user.  PlayerID binary address / port is written to the stream
+	ID_NAT_TARGET_NOT_CONNECTED,
+
+	// [NAT Punchthrough]
+	ID_NAT_CONNECT_AT_TIME,
+
+	// [NAT Punchthrough]
+	ID_NAT_SEND_OFFLINE_MESSAGE_AT_TIME,
+
+	// [Database] Internal
+	ID_DATABASE_QUERY_REQUEST,
+
+	// [Database] Internal
+	ID_DATABASE_UPDATE_ROW,
+
+	// [Database] Internal
+	ID_DATABASE_REMOVE_ROW,
+
+	// [Database] A serialized table.  Bytes 1+ contain the table.  Pass to TableSerializer::DeserializeTable
+	ID_DATABASE_QUERY_REPLY,
+
+	// [Database] Specified table not found
+	ID_DATABASE_UNKNOWN_TABLE,
+
+	// [Database] Incorrect password
+	ID_DATABASE_INCORRECT_PASSWORD,
+
+	// RakPeer - Downloading a large message. Format is ID_DOWNLOAD_PROGRESS (MessageID), partCount (unsigned int), partTotal (unsigned int), partLength (unsigned int), first part data (length <= MAX_MTU_SIZE)
+	ID_DOWNLOAD_PROGRESS,
+
+	///
+	/// SA-MP 0.3.7 Packet IDs
+	///
+
 	ID_VEHICLE_SYNC = 200,
-	ID_RCON_COMMAND = 201,
-	ID_RCON_RESPONCE = 202,
-	ID_AIM_SYNC = 203,
-	ID_WEAPONS_UPDATE = 204,
-	ID_STATS_UPDATE = 205,
-	ID_BULLET_SYNC = 206,
+	ID_RCON_COMMAND,
+	ID_RCON_RESPONCE,
+	ID_AIM_SYNC,
+	ID_WEAPONS_UPDATE,
+	ID_STATS_UPDATE,
 	ID_PLAYER_SYNC = 207,
-	ID_MARKERS_SYNC  = 208,
 	ID_UNOCCUPIED_SYNC = 209,
-	ID_TRAILER_SYNC = 210,
-	ID_PASSENGER_SYNC = 211,
-	ID_SPECTATOR_SYNC = 212,
+	ID_TRAILER_SYNC,
+	ID_PASSENGER_SYNC,
+	ID_SPECTATOR_SYNC,
 };
 
 #endif
