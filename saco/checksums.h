3b7b6e47c5d8e0191bbf1dfd0da5d19e7de2b6bf
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-04-17 22:16:29 +0800

    [saco] Implement and match checksum functions

--- saco/checksums.h
+++ saco/checksums.h
@@ -3,6 +3,11 @@
 
 //----------------------------------------------------------
 
+unsigned int CalcCRC32(unsigned int crc, const void *buf, size_t size);
+unsigned int CalcFileCRC32(char *filename);
+
+void CalcFileSHA1(char *filename, unsigned int *digest);
 void CalcSHA1(char *data, unsigned int len, unsigned int *digest);
+char *GetSHA1Result(unsigned int *digest, char *buf);
 
 //----------------------------------------------------------
