0b2cab3b184f83ec2e4bba9fd0ebc058ca7dab04
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-06-30 22:29:19 +0800

    [bot] Implement all util functions

---
+++ bot/runutil.h
@@ -0,0 +1,17 @@
+
+//----------------------------------------------------
+
+void Util_UrlUnencode(char *enc);
+char Util_toupper(char c);
+char *Util_stristr(const char *String, const char *Pattern);
+void Util_strupr(char *string);
+int Util_wildcmp(char *wild, char *string);
+int Util_strnicmp(const char *s1, const char *s2, size_t n);
+char *Util_strrev(char *str);
+char * Util_itoa(int v, char *s, int r);
+void Util_Base64Encode( char *cpInput, char *cpOutput );
+bool ContainsInvalidNickChars(char * szString);
+void ReplaceBadChars(char * szString);
+int CanFileBeOpenedForReading(char * filename);
+
+//----------------------------------------------------
