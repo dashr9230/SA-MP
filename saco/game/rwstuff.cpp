5082033f0cd24f8f0a25bfc3f625a21b7dd4ffc6
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-08-04 22:31:36 +0800

    [saco] Implement/match `RpWorldAddClump(...)`

--- saco/game/rwstuff.cpp
+++ saco/game/rwstuff.cpp
@@ -195,6 +195,19 @@ void RpClumpRender(RpClump *clump)
 	_asm pop edx
 }
 
+void RpWorldAddClump(RpClump *clump)
+{
+	DWORD dwWorld = *(DWORD*)0xC17038;
+	if (!dwWorld) return;
+
+	_asm push clump
+	_asm push dwWorld
+	_asm mov edx, 0x751300
+	_asm call edx
+	_asm pop edx
+	_asm pop edx
+}
+
 void RwCameraSetClipPlane(RwCamera *camera, float farClip, float nearClip)
 {
 	DWORD dwRwCameraSetFarClipPlane;
--- saco/game/rwstuff.h
+++ saco/game/rwstuff.h
@@ -27,6 +27,7 @@ void RwCameraEndUpdate(RwCamera *camera);
 void RwObjectHasFrameSetFrame(RwCamera *camera, RwFrame *frame);
 void SetCameraFrameAndZBuffer(RwCamera *camera, RwRaster *frameBuffer, RwRaster *zBuffer);
 void RpClumpRender(RpClump *clump);
+void RpWorldAddClump(RpClump *clump);
 void RwCameraSetClipPlane(RwCamera *camera, float farClip, float nearClip);
 void RwCameraSetViewWindow(RwCamera *camera, VECTOR2D *viewWindow);
 RpLight* RpLightCreate(int type);
