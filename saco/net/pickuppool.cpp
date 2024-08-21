de33537fc5b51fa43e74baebf95be0d3e4198444
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-08-21 23:25:26 +0800

    [saco] Move CPickupPool constructor to pickuppool.h

--- saco/net/pickuppool.cpp
+++ saco/net/pickuppool.cpp
@@ -3,20 +3,6 @@
 
 //----------------------------------------------------
 
-CPickupPool::CPickupPool()
-{
-	 memset(field_F004, 0, sizeof(field_F004));
-	 field_0 = 0;
-	 for (int i = 0; i < MAX_PICKUPS; i++)
-	 {
-		m_dwHnd[i] = NULL;
-		field_8004[i] = 0;
-		field_4004[i] = -1;
-	 }
-}
-
-//----------------------------------------------------
-
 CPickupPool::~CPickupPool()
 {
 	for(int i = 0; i < MAX_PICKUPS; i++)
--- saco/net/pickuppool.h
+++ saco/net/pickuppool.h
@@ -20,7 +20,17 @@ private:
 
 public:
 
-	CPickupPool();
+	CPickupPool() {
+		memset(field_F004, 0, sizeof(field_F004));
+		field_0 = 0;
+		for (int i = 0; i < MAX_PICKUPS; i++)
+		{
+			m_dwHnd[i] = NULL;
+			field_8004[i] = 0;
+			field_4004[i] = -1;
+		}
+	}
+
 	~CPickupPool();
 
 };
